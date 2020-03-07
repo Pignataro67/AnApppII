d5747f13a8a766a125417123c5ca7964d8d302c4
Pignataro67 <Pignataro67@gmail.com>
2020-03-07 05:41:17 -0800

    adding cont.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,11 @@
 class SearchController < ApplicationController
 
-    def search_start
+    def search_locations
       input = params[:input]
       response = RestClient::Request.execute(
         method: :get,
         url: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{input}&key=#{ENV['GOOGLE_PLACES_KEY']}",
         )
       render json: response
-    end
-  
-    def search_destination
-  
     end
   end 
