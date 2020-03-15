fb040fdbe95e4324dd9d917211ce257c6f866ed1
Pignataro67 <Pignataro67@gmail.com>
2020-03-15 09:32:42 -0700

    passn' props, syntax

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import SearchInput from "./SearchInput";
 import Button from "../Button";
 import Card from "../Card";
--- client/src/components/search/SearchInput.js
+++ client/src/components/search/SearchInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SearchInput = () => {
-  <input type='text' />
+const SearchInput = (props) => {
+  <Input action={{ icon: 'search' }} placeholder={props.label} onChange={ (e, data) => props.onChange(data)} />
 }
 
 export default SearchInput;
