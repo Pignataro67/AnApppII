d4cd9d65ce2e7b624fbaa00b11d2e39601e71061
Pignataro67 <Pignataro67@gmail.com>
2020-03-16 06:33:06 -0700

    adding connect and reducer

--- client/src/components/search/SearchInput.js
+++ client/src/components/search/SearchInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SearchInput = (props) => {
-  <Input action={{ icon: 'search' }} placeholder={props.label} onChange={ (e, data) => props.onChange(data)} />
+  <Input action={{ icon: 'search',  onClick: (e) => {props.onClick()} }} placeholder={props.label} onChange={ (e, data) => props.onChange(data)} />
 }
 
 export default SearchInput;
--- client/src/containers/searchContainer.js
+++ client/src/containers/searchContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Search from '../components/search/Search';
 import CardLabel from '../components/CardLabel';
+import * as searchActions from '../actions/fetchLocations';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class SearchContainer extends Component{
 
@@ -15,8 +18,19 @@ class SearchContainer extends Component{
   };
 }
 
-mapStateToProps = () => {
+const mapStateToProps = state => ({
+  startingLocation: state.startingLocation,
+  destination: state.destination
+})
 
+const mapDispatchToProps = (dispatch) => {
+  return {...bindActionCreators(searchActions, dispatch)}
 }
 
-export default SearchContainer; 
+export default connect(mapStateToProps, mapDispatchToProps, (stateProps, dispatchProps, ownProps) => {
+  return {
+    ...stateProps,
+    router: ownProps,
+    actions: dispatchProps,
+  };
+})(SearchContainer); 
