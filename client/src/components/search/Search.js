a2605afefcf15e6ad7829e661bec03e0cd53c230
Pignataro67 <Pignataro67@gmail.com>
2020-03-17 11:00:22 -0700

    improving func.

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -10,35 +10,48 @@ class Search extends Component {
     destination: ''
 }
 
-handleFormSubmit = () => {
-  console.log(this.state)
+handleFormSubmit = (e) => {
+  e.preventDefault()
   this.props.actions.fetchStartingLocation(this.state.startingLocation)
 }
 
-handleChangeStart = ({value: startingLocation}) => {
+handleChangeStart = (e) => {
   this.setState({
-      startingLocation
+      startingLocation: e.target.value,
   })
 }
 
-handleChangeDestination = ({value: destination}) => {
+handleChangeDestination = (e, {value: destination}) => {
   this.setState({
     destination
   })
 }
 
-handleStartSearch = () => {
-  console.log(this.state.startingLocation)
+handleStartSearch = (e) => {
+  e.preventDefault()
+
 }
 
-handleDestinationSearch = () => {
+handleDestinationSearch = (e) => {
   console.log(this.state.destination)
+  e.stopPropagation()
+  this.props.actions.fetchStartingLocation(this.state.startingLocation)
+}
+
+handleUpdateAddress = (e) => {
+  this.setState({
+      startingLocation: e.currentTarget.innerText,
+  })
 }
 
   render() {
     return (
       <Card >
-        <SearchInput label="Starting Location..." onChange={this.handleChangeStart} onClick={this.handleStartSearch}/>
+        <SearchInput label="Starting Location..." />
+        suggestedLocations={this.props.suggestedStartingLocations}
+        onChange={this.handleChangeStart} 
+        onSubmit={this.handleStartSearch}
+        handleUpdateAddress={this.handleUpdateAddress}/>
           <br/> 
           <br/>
         <SearchInput label="Destination..." onChange={this.handleChangeDestination} onClick={this.handleDestinationSearch}/> 
